d79413f50be46671d31ff604d0ae1e4107463b98
Shaurya Awasthi <shaurya.awasthi130@gmail.com>
2025-08-29 00:55:34 +0530

    cors updated

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",   // MUST match frontend
+  origin: process.env.CORS_ORIGIN,   // MUST match frontend
   credentials: true,                 // Allow cookies/sessions
 }));
 
